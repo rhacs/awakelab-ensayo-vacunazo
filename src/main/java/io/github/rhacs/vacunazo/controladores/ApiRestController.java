c2c7068d7bd129900940def33df971239f79f8aa
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-26 20:47:34 -0400

    added method buscarDoctoresPorEspecialidad

--- src/main/java/io/github/rhacs/vacunazo/controladores/ApiRestController.java
+++ src/main/java/io/github/rhacs/vacunazo/controladores/ApiRestController.java
@@ -1,15 +1,18 @@
 package io.github.rhacs.vacunazo.controladores;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Order;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.rhacs.vacunazo.modelos.Doctor;
 import io.github.rhacs.vacunazo.modelos.Especialidad;
 import io.github.rhacs.vacunazo.repositorios.DoctoresRepositorio;
 import io.github.rhacs.vacunazo.repositorios.EspecialidadesRepositorio;
@@ -46,6 +49,32 @@ public class ApiRestController {
     @GetMapping(path = "/especialidades")
     public List<Especialidad> obtenerTodas() {
         return especialidadesRepositorio.findAll(Sort.by(Order.asc("descripcion")));
+    }
+
+    /**
+     * Muestra el listado de {@link Doctor}es de acuerdo a la {@link Especialidad}
+     * proporcionada
+     * 
+     * @param id identificador numérico de la {@link Especialidad}
+     * @return un objeto {@link List} con el resultado, {@code null} en cualquier
+     *         otro caso
+     */
+    @GetMapping(path = "/doctores/especialidad/{id:^[0-9]+$}")
+    public List<Doctor> buscarDoctoresPorEspecialidad(@PathVariable Long id) {
+        // Buscar información de la Especialidad
+        Optional<Especialidad> especialidad = especialidadesRepositorio.findById(id);
+
+        // Verificar si existe
+        if (especialidad.isPresent()) {
+            // Buscar listado de doctores que tienen la especialidad especificada
+            List<Doctor> doctores = doctoresRepositorio.findByEspecialidad(especialidad.get());
+
+            // Devolver listado
+            return doctores;
+        }
+
+        // Devolver null si la especialidad no existe
+        return null;
     }
 
 }
