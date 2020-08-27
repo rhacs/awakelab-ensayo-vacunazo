189a3902152c25b3b61880255b97eb8fbb11ce08
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-27 18:21:26 -0400

    added method findByRutPaciente

--- src/main/java/io/github/rhacs/vacunazo/repositorios/PacientesRepositorio.java
+++ src/main/java/io/github/rhacs/vacunazo/repositorios/PacientesRepositorio.java
@@ -16,4 +16,12 @@ public interface PacientesRepositorio extends JpaRepository<Paciente, Long> {
      */
     public Optional<Paciente> findTopByOrderByIdDesc();
 
+    /**
+     * Busca un registro en el repositorio de {@link Paciente}s
+     * 
+     * @param rutPaciente rol único tributario a buscar
+     * @return un objeto {@link Optional} con el resultado
+     */
+    public Optional<Paciente> findByRutPaciente(String rutPaciente);
+
 }
