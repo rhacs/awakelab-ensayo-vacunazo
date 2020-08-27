7b41afe614ff71d91f63e10fa69d0756165bde4e
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-26 22:24:45 -0400

    refactoring

--- src/main/webapp/resources/js/formulario.js
+++ src/main/webapp/resources/js/formulario.js
@@ -1,27 +1,35 @@
 // Esperar a que cargue la página
 $(function() {
     // Asignar el evento 'change' al select correspondiente a las Especialidades
-    $('#doctor.especialidad.id').on('change', function() {
+    $('#doctor\\.especialidad').change(function() {
         // Obtener el valor del elemento seleccionado
         let id = $(this).children('option:selected').val();
 
         // Obtener listado de doctores que pertenecen a la especialidad
         $.get('http://localhost/vacunazo/api/doctores/especialidad/' + id, function(data) {
             // Limpiar el select correspondiente a los Doctores
-            $('#doctor.id').empty();
+            $('#doctor').empty();
 
             // Por cada elemento de la respuesta
             $.each(data, function() {
                 // Generar una nueva opción
                 let opcion = $('<option />')
-                        // Establecer valor
-                        .val(this.id)
-                        // Establecer etiqueta
-                        .text(this.nombre + ' ' + this.apellido);
+                    // Establecer valor
+                    .val(this.id)
+                    // Establecer etiqueta
+                    .text(this.nombreCompleto);
 
                 // Agregar opción al select
-                $('#doctor.id').append(opcion);
+                $('#doctor').append(opcion);
             });
+
+            // Seleccionar el doctor correspondiente
+            if (doc != -1) {
+                $('#doctor option[value="' + doc + '"]').attr('selected', true);
+            }
         });
     });
+
+    // Forzar evento 'change'
+    $('#doctor\\.especialidad').change();
 });
